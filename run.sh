1d8a36c2a4d818d80113bd46b4ce7ec92daca78e
Daniele Muffato <danixgithub1@gmail.com>
2025-04-10 20:52:43 +0200

    HELP, REPO env vars impl

--- run.sh
+++ run.sh
@@ -3,3 +3,34 @@
 SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
 DATA_DIR="${SCRIPT_DIR}/data"
+
+if [[ -v HELP ]]; then
+    echo -n 'script to auto manage different repos to track payments:
+
+    Environment variables:
+HELP=       print this help message
+REPO=       specify the repository to use
+    -> values: go/golang, rust
+'
+    exit 0
+fi
+
+[[ -v REPO ]] || REPO="golang"
+case "${REPO}" in
+go | golang)
+    REPO_URL="https://github.com/daniele821/track-payments-golang"
+    REPO_DIR="${DATA_DIR}/.repo_golang"
+    ;;
+rust)
+    REPO_URL="https://github.com/daniele821/track-payments-rust"
+    REPO_DIR="${DATA_DIR}/.repo_rust"
+    ;;
+*)
+    echo "invalid REPO value ($REPO)"
+    exit 1
+    ;;
+esac
+
+if [[ ! -d "$REPO_DIR" ]]; then
+    git clone "$REPO_URL" "$REPO_DIR"
+fi
